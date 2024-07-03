adbe974c9498fd9a5e026471f9c8fee127538b1b
DARSAN <iamspdarsan@gmail.com>
2024-07-03 08:18:31 +0530

    style correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
+	content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./meta.tsx"],
 	theme: {
 		extend: {
 			fontFamily: {
